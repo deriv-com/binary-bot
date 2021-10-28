fb177518d9fdd8d43cfade6b1eebba4b219c4a75
nikolai-deriv <nikolai@deriv.com>
2021-10-28 15:41:22 +0330

    replace date methods with UTC equivalents

--- src/botPage/view/deriv/layout/Footer/components/server-time.jsx
+++ src/botPage/view/deriv/layout/Footer/components/server-time.jsx
@@ -13,11 +13,11 @@ const ServerTime = ({ api }) => {
         date.setSeconds(date.getSeconds() + 1);
 
         const year = date.getUTCFullYear();
-        const month = `0${date.getMonth() + 1}`.slice(-2);
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
         const day = `0${date.getUTCDate()}`.slice(-2);
         const hours = `0${date.getUTCHours()}`.slice(-2);
-        const minutes = `0${date.getMinutes()}`.slice(-2);
-        const seconds = `0${date.getSeconds()}`.slice(-2);
+        const minutes = `0${date.getUTCMinutes()}`.slice(-2);
+        const seconds = `0${date.getUTCSeconds()}`.slice(-2);
 
         setDateString(`${year}-${month}-${day} ${hours}:${minutes}:${seconds} GMT`);
     };
