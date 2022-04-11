a3a287b2a4923caced3c528924cb3a9708e971eb
mehrzad-fs <mehrzad@firstsource.tech>
2022-04-11 19:39:53 +0430

    move view to src

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require("webpack");
 const $ = require("jquery");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "botPage", "view", "index.js"),
+  entry: path.join(__dirname, "src", "view", "index.js"),
   output: {
     filename: "[name].[hash].js",
     path: path.join(__dirname, "dist"),
@@ -14,18 +14,18 @@ module.exports = {
   },
   resolve: {
     alias: {
-      Styles: path.resolve(__dirname, "src/botPage/view/styles"),
+      Styles: path.resolve(__dirname, "src/view/styles"),
       Static: path.resolve(__dirname,'static'),
       Translate: path.resolve(__dirname,"src","common","i18n.js"),
       StorageManager: path.resolve(__dirname,"src","common","utils","storageManager.js"),
       Observer: path.resolve(__dirname,"src","common","utils","observer.js"),
       Tools: path.resolve(__dirname,"src","common","utils","tools.js"),
       Common: path.resolve(__dirname,"src", "common"),
-      CommonDeriv: path.resolve(__dirname,"src","botPage", "common"),
-      BlocklyPath: path.resolve(__dirname,"src","botPage","view","blockly"),
-      Api: path.resolve(__dirname,"src","botPage","view","deriv","api"),
-      Store:path.resolve(__dirname,"src","botPage","view","deriv","store"),
-      Components: path.resolve(__dirname,"src","botPage","view","deriv","components"),
+      CommonDeriv: path.resolve(__dirname,"src", "common"),
+      BlocklyPath: path.resolve(__dirname,"src","view","blockly"),
+      Api: path.resolve(__dirname,"src","view","deriv","api"),
+      Store:path.resolve(__dirname,"src","view","deriv","store"),
+      Components: path.resolve(__dirname,"src","view","deriv","components"),
       Bot: path.resolve(__dirname,"src","bot"),
       Shared: path.resolve(__dirname,"src","shared"),
     },
