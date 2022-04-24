6c9d1c6b264893031cf90697128741f0fefd7051
mehrzad-fs <mehrzad@firstsource.tech>
2022-04-24 12:44:15 +0430

    update colors scss

--- src/view/index.js
+++ src/view/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import "notifyjs-browser";
 import "jquery-ui/ui/widgets/dialog";
 import store from "Store";
-import 'Styles/color.scss'
 import 'Styles/chart.scss';
 import 'Styles/bot.scss';
 import App from './app'
