e7d8394c2201f39e8bac3ea8d64107a43f29cd5d
Sandeep <sandeep@deriv.com>
2022-06-16 17:22:18 +0800

    reverted balance subscribe changes

--- src/botPage/view/deriv/layout/Header/Header.jsx
+++ src/botPage/view/deriv/layout/Header/Header.jsx
@@ -100,6 +100,7 @@ const Header = () => {
           api.send({
             balance: 1,
             account: "all",
+            subscribe: 1,
           })
             .then(() => { })
             .catch(() => { });
