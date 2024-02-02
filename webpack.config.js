e5aa8bfff1a7acf2c5056539a027392c039d1389
Farabi Chowdhury <farabi@deriv.com>
2024-02-02 14:52:12 +0800

    fix: css variable

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        port: 8004,
+        port: 80,
         host: 'localbot.binary.sx',
         open: true,
         historyApiFallback: true,
