b3b829ca9c477c780bc5c2166b92aa0ebb0c889d
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-02-15 15:12:18 +0300

    fix: Trader's hub icon navigation should be Wallet (https://app.deriv.com/wallets)

--- src/components/Header/menu-links.jsx
+++ src/components/Header/menu-links.jsx
@@ -6,7 +6,7 @@ const MenuLinks = () => (
     <div className='header__menu-item header__menu-links client_logged_in'>
         {config.reports.visible && (
             <div>
-                <a className='url-reports-positions header__menu-links-item' href={config.tradershub.url}>
+                <a className='url-reports-positions header__menu-links-item' href={config.wallets.url}>
                     <div className='header__icon-container'>
                         <img className='header__icon-text reports-icon' src='/public/images/traders_hub.png' />
                     </div>
