673c4af32e6653aa3afe0f8ce7424884e897c7bb
vinu-deriv <vinu@deriv.com>
2024-03-06 18:06:04 +0400

    fix: remove version number for datadog

--- src/utilities/utility-functions.js
+++ src/utilities/utility-functions.js
@@ -13,10 +13,3 @@ export const debounce = (func, delay) => {
 
     return debouncedFunction;
 };
-
-export const formatDate = date => {
-    const year = date.getFullYear();
-    const day = `0${date.getDate()}`.slice(-2);
-    const month = `0${date.getMonth() + 1}`.slice(-2);
-    return year + day + month;
-};
