d49863d209213b03bde848d7b9932b279f3ec902
Farabi Chowdhury <farabi@deriv.com>
2024-03-25 12:00:06 +0800

    fix: add staging redirection

--- src/components/Banner/redirect-to-dbot.js
+++ src/components/Banner/redirect-to-dbot.js
@@ -4,5 +4,9 @@ import { getActiveLoginId } from '@storage';
 export const visitDerivBot = () => {
     const user_id = getActiveLoginId();
     GTM.pushDataLayer({ event: 'bbot_moved_to_deriv_bot', user_id: user_id ?? null });
+    const isStaging = process.env.NODE_ENV === 'staging';
+    if (isStaging) {
+        window.open('https://staging-app.deriv.com/bot/?redirect_from_bbot=1', '_self', 'noopener');
+    }
     window.open('https://app.deriv.com/bot/?redirect_from_bbot=1', '_self', 'noopener');
 };
